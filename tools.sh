730094189b66565cd8ffbf65e7093f5644bedcf6
Luís Gonçalves <luis.fgoncalv@gmail.com>
2016-09-26 22:56:31 +0100

    Add mono

--- tools.sh
+++ tools.sh
@@ -21,6 +21,13 @@ sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
 sudo apt-get update
 sudo apt-get install -y dotnet-dev-1.0.0-preview2-003131
 
+# mono
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
+sudo apt-get install -y mono-complete
+sudo apt-get install -y mono-xsp4
+
 # vscode
 wget https://go.microsoft.com/fwlink/?LinkID=760868 -qO vscode.deb
 sudo dpkg -i vscode.deb
